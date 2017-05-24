b4906d2fb95697c36ac4c5a98ef3ebca72ab99ff
Daniel Tan <dtan@tripadvisor.com>
2017-05-24 23:26:36 +1000

    don't understand

---
+++ lifeguard.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/python2.7
+"""
+You’re a lifeguard standing on the beach, 
+right at the edge of the water, and gazing out over the ocean. 
+You see someone drowning 100 meters to the right of you and 100 meters away from shore. 
+You can run 100 meters in 15 seconds and swim 100 meters in 75 seconds. 
+(The beach drops off steeply, meaning that you can’t run in the water.) 
+What’s the fastest you can get to the victim?
+"""
