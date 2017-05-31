827daf2cebc409e5d4a0ccf8290139cddf1e592c
Daniel Tan <dtan@tripadvisor.com>
2017-05-31 18:21:46 +0800

    managing folders

---
+++ daily-programmer/277-easy/fraction.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2.7
+"""
+://www.reddit.com/r/dailyprogrammer/comments/4uhqdb/20160725_challenge_277_easy_simplifying_fractions/
+"""
+def main():
+
+
+if __name__ == "__main__":
+    main()
+
