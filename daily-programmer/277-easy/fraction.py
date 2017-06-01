dad5e25b8d7cf4691871860d2d563e0c354381ec
Daniel Tan <dtan@tripadvisor.com>
2017-06-01 10:54:23 +0800

    solved but had to refer to online solution

--- daily-programmer/277-easy/fraction.py
+++ daily-programmer/277-easy/fraction.py
@@ -2,9 +2,26 @@
 """
 ://www.reddit.com/r/dailyprogrammer/comments/4uhqdb/20160725_challenge_277_easy_simplifying_fractions/
 """
-def main():
+import sys
 
+def main(num,den):
+    #find greatest common divisor using Euclid's Algorithm
+    #gcd(a,b)
+    #  gcd(a,0)=a 
+    #  gcd(a,b)=gcd(b,a %b)
+    a = num
+    b = den
+    while b: #ie. while b != 0
+        a,b = a, a%b
+    print num/a, den/a
 
 if __name__ == "__main__":
-    main()
+    # fraction.py num den
+    if len(sys.argv) !=3:
+        print 'Usage: python fraction.py numerator denominator'
+        sys.exit()
+    else:
+        num = int(sys.argv[1])
+        den = int(sys.argv[2])
+        main(num,den)
 
