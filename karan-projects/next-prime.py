91a8e116f37f6c5795c95f8ccb7f8276132cc7f9
Daniel Tan <dtan@tripadvisor.com>
2017-06-13 21:50:12 +0800

    learning to test with assert

---
+++ karan-projects/next-prime.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+"""
+Next Prime Number - Have the program find prime numbers until the user chooses to stop asking for the next one.
+"""
+import sys
+
+def isprime(number):
+    if number==0: #0 is not prime
+        return False
+    elif number==1:
+        return True
+    elif number==2:
+        return True
+    elif number%2==0: #all even numbers are not prime
+         return False
+    else:
+        for i in range(3,number-2,2): #check 3,5,7,9...
+            if number%i==0:
+                return False
+    return True
+
+def test1():
+    check = isprime(7)
+    assert check==True
+
+def test2():
+    check = isprime(28)
+    assert check==False
+
+def main():
+    print 'test'
+
+if __name__ == "__main__":
+    #main()
+    print isprime(7)
+    print test1()
+    print test2()
+
