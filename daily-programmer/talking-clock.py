f88e984f313df0f249a3d3eecb6c695a26b4e4cd
Daniel Tan <dtan@tripadvisor.com>
2017-07-04 17:47:28 +0800

    trying to get the habit started again

---
+++ daily-programmer/talking-clock.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+"""
+https://www.reddit.com/r/dailyprogrammer/comments/6jr76h/20170627_challenge_321_easy_talking_clock/
+Sample Input data
+00:00
+01:30
+12:05
+14:01
+20:29
+21:00
+
+Sample Output data
+It's twelve am
+It's one thirty am
+It's twelve oh five pm
+It's two oh one pm
+It's eight twenty nine pm
+It's nine pm
+"""
+def main():
+    num2words={1:'One',2:'Two',3:'Three'
+            }
+
+if __name__ == "__main__":
+    main()
+
