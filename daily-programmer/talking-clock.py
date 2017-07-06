20099f54180ffd5c04ba040294243e30a52c2ef3
Daniel Tan <dtan@tripadvisor.com>
2017-07-06 17:23:19 +0800

    relearning dicts

--- daily-programmer/talking-clock.py
+++ daily-programmer/talking-clock.py
@@ -18,10 +18,39 @@ It's two oh one pm
 It's eight twenty nine pm
 It's nine pm
 """
-def main():
-    num2words={1:'One',2:'Two',3:'Three'
+import sys
+
+def main(user_input):
+    #Dictionary
+    num2words={1:'One',2:'Two',3:'Three', 4:'Four', 5:'Five',
+            6:'Six',7:'Seven',8:'Eight',9:'Nine',10:'Ten',
+            11:'Eleven',12:'Twelve',
+            20:'Twenty',30:'Thirty',40:'Forty',50:'Fifty',60:'Sixty'
             }
 
+    #Parse user input into HH and mm
+    HH, mm = user_input.split(':')
+    HH = int(HH)
+    mm = int(mm)
+
+    if HH>=12:
+        ampm='pm'
+        HH -=12
+    else:
+        ampm='am'
+    hour = num2words.get(HH)
+    if mm>12:
+        first_digit = mm/10
+        back_digit = mm%10
+        minute = num2words.get(first_digit) + '-' + num2words.get(back_digit)
+    else:
+        minute = num2words.get(mm)
+
+    #Print output
+    output = "It's {} {} {}".format(hour,minute,ampm)
+    print output
+
+
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
 
